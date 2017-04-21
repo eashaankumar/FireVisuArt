5d51508bf677fd194409c97529ac36de4c34000c
Eashaan Kumar <eashaan@kumar.co>
2017-04-21 16:14:36 -0400

    animations

---
+++ VisuArt/Assets/Scripts_Obj/ArtObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtObject : MonoBehaviour {
+
+    private MeshRenderer renderer;
+    private Rigidbody rigid;
+    public ArtObjectMode artObjMode;
+
+	// Use this for initialization
+	void Awake () {
+        renderer = GetComponent<MeshRenderer>();
+		if (renderer == null) {
+			renderer = gameObject.GetComponentInChildren<MeshRenderer> ();
+		}
+        Hide();
+	}
+	
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void ArtSpawnMode()
+    {
+        artObjMode = ArtObjectMode.SPAWN_MODE;
+		/*foreach (Material mat in renderer.materials){
+			mat.color = new Color (1, 1, 1, 0.5f);
+		}*/
+    }
+
+    public void ArtNormalMode()
+    {
+        artObjMode = ArtObjectMode.NORMAL_MODE;
+        /*foreach (Material mat in renderer.materials){
+			mat.color = new Color (1, 1, 1, 0.5f);
+		}*/
+        //renderer.material.color = ;
+    }
+
+    public ArtObjectMode GetArtMode()
+    {
+        return artObjMode;
+    }
+}
+
+public enum ArtObjectMode
+{
+    SPAWN_MODE, NORMAL_MODE
+}
---
+++ VisuArt/Assets/Scripts_Obj/FlickeringLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Light))]
+public class FlickeringLights : MonoBehaviour {
+
+	private Light light;
+	private float meanLightIntensity;
+	public float speed;
+	// Use this for initialization
+	void Awake () {
+		light = GetComponent<Light> ();
+	}
+
+	void Start(){
+		meanLightIntensity = light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.intensity = Mathf.Lerp(light.intensity, Mathf.Sin (Time.deltaTime * speed) * 2 + meanLightIntensity, 0.5f);
+	}
+}
