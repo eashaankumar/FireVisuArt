77dd4834bcee2f1f0c5d2cf9faaee7980a1aa3a3
Eashaan Kumar <eashaan@kumar.co>
2017-05-05 11:37:07 -0400

    Final version

---
+++ VisuArt/Assets/_Scripts/Scripts_Line/ColorManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorManager : MonoBehaviour {
+	public static ColorManager Instance;
+	void Awake(){
+		if (Instance == null) {
+			Instance = this;
+		}
+	}
+	void OnDestroy(){
+		if (Instance == this) {
+			Instance = null;
+		}
+	}
+
+	Color color;
+
+	void OnColorChange(HSBColor color) 
+	{
+		print (color);
+		this.color = color.ToColor();
+	}
+	public Color GetCurrentColor() {
+		return this.color;
+	}
+}
---
+++ VisuArt/Assets/_Scripts/Scripts_Obj/RevolveCenter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof (ArtObject))]
+
+public class RevolveCenter : MonoBehaviour {
+
+    public float angle;
+    public int direction;
+
+    private ArtObject artObj;
+
+	// Use this for initialization
+	void Awake () {
+        artObj = GetComponent<ArtObject>();
+	}
+
+	void Start(){
+		float random = Random.value;
+		if (random <= 0.5)
+			direction = -1;
+		else
+			direction = 1;
+		angle = Random.Range (0f, 60);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (artObj == null)
+            return;
+        if(artObj.GetArtMode() == ArtObjectMode.NORMAL_MODE)
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, angle * direction * Time.deltaTime);
+        }
+    }
+}
