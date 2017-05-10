8e3843541374779664287841e7764960fce7bbca
Eashaan Kumar <eashaan@kumar.co>
2017-05-10 13:41:58 -0400

    ip

---
+++ VisuArtMult/Assets/NetManagerCuston.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetManagerCuston : NetworkManager {
+
+	public void JoinGameButton(){
+		string hostAddress = Network.player.ipAddress + "";
+		base.networkAddress = hostAddress;
+		base.StartClient ();
+		print("Start Client");
+	}
+}
